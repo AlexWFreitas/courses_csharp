bb3e9c4a1902fdd16262b123e530c63baaa8807d
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-11 14:02:05 -0300

    Testing different types of throws.

--- error-handling-csharp-exceptions/Exceptions/Sandbox/Program.cs
+++ error-handling-csharp-exceptions/Exceptions/Sandbox/Program.cs
@@ -6,7 +6,35 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                DoSomeUsefulWork();
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+        }
+
+        private static void DoSomeUsefulWork()
+        {
+            try
+            {
+                ICanThrowException();
+                ICanThrowException();
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+                throw;
+            }
+        }
+
+        private static void ICanThrowException()
+        {
+            throw new Exception("Bad thing happened");
+        }
+
+        private static void Log(Exception ex)
+        {
+            // Intentionally left blank
         }
     }
 }
