cf65a5899ef154489efa9b95f02cad8c3c0e8b87
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-12 17:34:48 -0300

    Error Handling in C# with Exceptions - Chapter 6 - 50%

---
+++ error-handling-csharp-exceptions/Exceptions/ConsoleCalculator/CalculationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    class CalculationException : Exception
+    {
+        private static readonly string DefaultMessage = "An error occurred during calculation. Ensure" +
+            " that the operator is supported and that the values are within valid ranges for the requested operation.";
+
+        
+        public CalculationException() : base(DefaultMessage)
+        {
+        }
+
+        public CalculationException(string message) : base(message)
+        {
+        }
+
+        public CalculationException(Exception ex) : base(DefaultMessage, ex)
+        {
+        }
+
+        public CalculationException(string message, Exception ex) : base(message, ex)
+        {
+        }
+
+
+    }
+}
