bb34b1ee17d2bc2ccf06c45afb470f97d013ca45
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-27 15:01:16 -0300

    Applying Asynchronous Programming in C# - Chapter 6 - Showing how Task delegates return results from Task.Run and Task.Factory.StartNew
    - Concept of automatically unwrapping Tasks.
    - Unwrap method
    - Returning a Task<T> and returning T inside a Task delegate.

--- async-csharp/exercise-files/06/demos/Windows/Start_Here/StockAnalyzer.AttachedDetached/Program.cs
+++ async-csharp/exercise-files/06/demos/Windows/Start_Here/StockAnalyzer.AttachedDetached/Program.cs
@@ -8,54 +8,70 @@ namespace StockAnalyzer.AttachedDetached
     {
         static async Task Main(string[] args)
         {
-            // Example related to awaited on a chain of Tasks whose results depend from the nested Tasks operations.
-            var x = await Run();
-            Console.WriteLine(x);
-
-
-            // Example related to Independent Nested Tasks being attached to the awaited Parent Task.
             Console.WriteLine("Starting");
 
-            await Task.Factory.StartNew(() =>
+            // Examples showing a few concepts
+            // async anonymous methods can return the Task.Result, but the returned value is always a Task.
+            // You generally return the Task.Result instead of a Task inside of a Task delegate.
+            // If you do return a Task instead of Task.Result from a Task method code, ... ...
+            // Task.Run unwraps the Task of Task of Result into Task of Result automatically for you
+            // Task.Factory.StartNew doesn't do this though.
+
+            Task<Task<string>> Task1 = Task.Factory.StartNew(async () =>
             {
-                Task.Factory.StartNew(() =>
-                {
-                    Thread.Sleep(1000);
+                await Task.Delay(2000);
 
-                    Console.WriteLine("Completed 1");
-                }, TaskCreationOptions.AttachedToParent);
-                Task.Factory.StartNew(() =>
-                {
-                    Thread.Sleep(2000);
-
-                    Console.WriteLine("Completed 2");
-                }, TaskCreationOptions.AttachedToParent);
-                Task.Factory.StartNew(() =>
-                {
-                    Thread.Sleep(3000);
-
-                    Console.WriteLine("Completed 3");
-                }, TaskCreationOptions.AttachedToParent);
+                return "PluralSight!";
             });
-            
+
+            Task<string> UnwrappedTask = Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(2000);
+
+                return "PluralSight!";
+            }).Unwrap(); // Unwrap method
+
+            Task<Task<string>> Task3a = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+
+                return Task.FromResult("PluralSight!");
+            });
+
+            Task<string> Task3 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+
+                return "PluralSight!";
+            });
+
+            Task<string> Task2 = Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+
+                return "PluralSight!";
+            });
+
+            var resultTask2 = await Task2;
+            Console.WriteLine(resultTask2);
+
+
+            Task<string> Task4 = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+
+                return "PluralSight!";
+            });
+
+            Task<string> Task4a = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+
+                return Task.FromResult("PluralSight!");
+            });
+
             Console.WriteLine("Completed");
             Console.ReadLine();
-        }
-
-        public static Task<string> Run()
-        {
-            return Task.Run(() => Compute());
-        }
-
-        public static Task<string> Compute()
-        {
-            Thread.Sleep(5000);
-            return Task.Run(() => Load());
-        }
-
-        public static Task<string> Load()
-        {
-            return Task.Run(() => "Even though I have awaited the top of the chain, it still properly validated every nested Task.");
         }
     }
 }
