dba62d5304f6f2dbcb87f650bc995b070b984828
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-28 08:57:08 -0300

    Applying Asynchronous Programming in C# - Extra - Added an example about Thread.Sleep

--- async-csharp/exercise-files/06/demos/Windows/Start_Here/ThreadSleep/Program.cs
+++ async-csharp/exercise-files/06/demos/Windows/Start_Here/ThreadSleep/Program.cs
@@ -8,32 +8,109 @@ namespace ThreadSleep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting");
+            Console.WriteLine("Starting on Original Thread - Sleeping for 250ms after launching other tasks");
 
             Task.Run(() =>
             {
+                Console.WriteLine("Started Task 1 on Thread 1 - Sleeping for 500ms after launching other tasks");
+
                 Task.Run(() =>
                 {
+                    Console.WriteLine("Started Task 1-1 on Thread 2 - Sleeping for 1000ms");
+
                     Thread.Sleep(1000);
 
-                    Console.WriteLine("Completed Thread 1");
+                    Console.WriteLine("Completed Task 1-1 on Thread 2");
                 });
                 Task.Run(() =>
                 {
+                    Console.WriteLine("Started Task 1-2 on Thread 3 - Sleeping for 2000ms");
+
                     Thread.Sleep(2000);
 
-                    Console.WriteLine("Completed Thread 2");
+                    Console.WriteLine("Completed Task 1-2 on Thread 3");
                 });
                 Task.Run(() =>
                 {
-                    Thread.Sleep(3000);
+                    Console.WriteLine("Started Task 1-3 on Thread 4 - Sleeping for 10000ms");
 
-                    Console.WriteLine("Completed Thread 3");
+                    Thread.Sleep(10000);
+
+                    Console.WriteLine("Completed Task 1-3 on Thread 4");
                 });
+
+                Thread.Sleep(500);
+
+                Console.WriteLine("Completed Task 1 on Thread 1");
             });
+
+            Thread.Sleep(250);
 
             Console.WriteLine("Completed Original Thread");
             Console.ReadLine();
+
+            Console.WriteLine("Starting on Original Thread - Sleeping for 5000ms after launching other tasks");
+
+            Task.Run(() =>
+            {
+                Console.WriteLine("Started Task 1 on Thread 1 - Sleeping for 4000ms after launching other tasks");
+
+                Task.Run(() =>
+                {
+                    Console.WriteLine("Started Task 1-1 on Thread 2 - Sleeping for 1000ms");
+
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Completed Task 1-1 on Thread 2");
+                });
+                Task.Run(() =>
+                {
+                    Console.WriteLine("Started Task 1-2 on Thread 3 - Sleeping for 2000ms");
+
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine("Completed Task 1-2 on Thread 3");
+                });
+                Task.Run(() =>
+                {
+                    Console.WriteLine("Started Task 1-3 on Thread 4 - Sleeping this Task for 10000ms");
+
+                    Thread.Sleep(10000);
+
+                    Console.WriteLine("Completed Task 1-3 on Thread 4");
+                });
+
+                Thread.Sleep(4000);
+
+                Console.WriteLine("Completed Task 1 on Thread 1");
+            });
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Completed Original Thread");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
