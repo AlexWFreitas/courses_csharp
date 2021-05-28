c9739fda9a74ac8867ad8a607364aa3488fe84b5
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-28 08:20:47 -0300

    Applying Asynchronous Programming in C# - Extra - Added an example about Thread.Sleep

---
+++ async-csharp/exercise-files/06/demos/Windows/Start_Here/ThreadSleep/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            Task.Run(() =>
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Completed Thread 1");
+                });
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine("Completed Thread 2");
+                });
+                Task.Run(() =>
+                {
+                    Thread.Sleep(3000);
+
+                    Console.WriteLine("Completed Thread 3");
+                });
+            });
+
+            Console.WriteLine("Completed Original Thread");
+            Console.ReadLine();
+        }
+    }
+}
