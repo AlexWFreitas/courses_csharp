d603bd6436edff587faa1388c3b67a5805b51696
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-31 15:58:35 -0300

    C# Interfaces Course - Chapter 4 - FakeRepository & PersonRepository.Fake & PeopleViewModel Unit Tests
    - Added PersonRepository.Fake project
    - Added FakeRepository class
    - Added PeopleViewModel Unit Tests
    - Refactored some of the code on those classes when compared to the Course examples.

--- csharp-interfaces-jc/demo-files/04/demos/before/DynamicLoading/PeopleViewer.Test/PeopleViewModelTest.cs
+++ csharp-interfaces-jc/demo-files/04/demos/before/DynamicLoading/PeopleViewer.Test/PeopleViewModelTest.cs
@@ -5,6 +5,53 @@ namespace PeopleViewer.Test
 {
     public class PeopleViewModelTest
     {
+        [Test]
+        public void People_OnFetchData_IsPopulated()
+        {
+            // Arrange
+            PeopleViewModel viewModel = new PeopleViewModel();
+            int expected = 2;
 
+            // Act
+            viewModel.FetchData();
+            int actual = viewModel.People.Count();
+
+            // Assert
+            Assert.IsNotNull(viewModel.People);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void People_OnClearData_IsEmpty()
+        {
+            // Arrange
+            PeopleViewModel viewModel = new PeopleViewModel();
+            viewModel.FetchData();
+            int expected = 0;
+
+            // Check Arrangement
+            Assert.AreEqual(2, viewModel.People.Count(), "Invalid arrangement.");
+
+            // Act
+            viewModel.ClearData();
+            int actual = viewModel.People.Count();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RepositoryType_OnCreation_ReturnsFakeRepositoryString()
+        {
+            // Arrange
+            PeopleViewModel viewModel = new PeopleViewModel();
+            string expected = "PersonRepository.Fake.FakeRepository";
+
+            // Act
+            string actual = viewModel.RepositoryType;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
